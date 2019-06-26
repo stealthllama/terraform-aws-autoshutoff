08c0a41524af558e694a9aa91ee760b7bde093c9
Michael Richardson <mrichardson03@gmail.com>
2019-06-26 09:58:30 -0400

    flake8

--- lambda_function.py
+++ lambda_function.py
@@ -5,15 +5,16 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
     logger.info('## EVENT')
     logger.info(event)
-    
+
     client = boto3.client('ec2')
     ec2_regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
-    
+
     tag_name = os.getenv('SHUTOFF_TAG_NAME')
     tag_value = os.getenv('SHUTOFF_TAG_VALUE')
 
@@ -30,13 +31,13 @@ def lambda_handler(event, context):
             'Values': ['pending', 'running']
         }
     ]
-    
+
     for region in ec2_regions:
         ec2 = boto3.resource('ec2',region_name=region)
         running_instances = [r for r in ec2.instances.filter(Filters=filter_running)]
         exempt_instances = [x for x in ec2.instances.filter(Filters=filter_exempt)]
         stop_instances = [s for s in running_instances if s.id not in [x.id for x in exempt_instances]]
-        
+
         for instance in stop_instances:
             result = instance.stop()
             logger.info(result)
